9912e4eb79091c2e6def3087f2c683c4f7b227c9
Owen Jones <owengalenjones@gmail.com>
2015-08-04 22:04:55 -0400

    lein

---
+++ lein/.lein/profiles.clj
@@ -0,0 +1,8 @@
+{:user
+ {:plugins
+  [[lein-marginalia "0.7.1"]
+   [lein-midje "3.1.3"]
+   [lein-pprint "1.1.1"]
+   [lein-deps-tree "0.1.2"]
+   [cider/cider-nrepl "0.7.0-SNAPSHOT"]
+   [com.jakemccrary/lein-test-refresh "0.10.0"]]}}
